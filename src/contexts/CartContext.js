0dedd952f0be0f9daca3d1f17bd954c5fb2d1683
Salma-Alfinda <salma.alfinda@students.utdi.ac.id>
2024-02-09 19:56:32 +0700

    first commit

---
+++ src/contexts/CartContext.js
@@ -0,0 +1,102 @@
+import React, { createContext, useState, useEffect } from "react";
+
+export const CartContext = createContext();
+
+const CartProvider = ({ children }) => {
+  const [cart, setCart] = useState([]);
+  const [itemAmount, setItemAmount] = useState(0);
+  const [total, setTotal] = useState(0);
+
+  useEffect(() => {
+    const calculateTotal = () => {
+      const total = cart.reduce((accumulator, currentItem) => {
+        const itemTotal = currentItem.price * currentItem.amount;
+        return isNaN(itemTotal) ? accumulator : accumulator + itemTotal;
+      }, 0);
+      setTotal(total);
+    };
+
+    calculateTotal();
+  }, [cart]);
+
+  useEffect(() => {
+    const calculateItemAmount = () => {
+      if (cart) {
+        const amount = cart.reduce((accumulator, currentItem) => {
+          return accumulator + currentItem.amount;
+        }, 0);
+        setItemAmount(amount);
+      }
+    };
+
+    calculateItemAmount();
+  }, [cart]);
+
+  const addToCart = (product, id) => {
+    const newItem = { ...product, amount: 1 };
+    const cartItem = cart.find((item) => item.id === id);
+
+    if (cartItem) {
+      const newCart = cart.map((item) => {
+        if (item.id === id) {
+          return { ...item, amount: cartItem.amount + 1 };
+        } else {
+          return item;
+        }
+      });
+      setCart(newCart);
+    } else {
+      setCart([...cart, newItem]);
+    }
+  };
+
+  const removeFromCart = (id) => {
+    const newCart = cart.filter((item) => item.id !== id);
+    setCart(newCart);
+  };
+
+  const clearCart = () => {
+    setCart([]);
+  };
+
+  const increaseAmount = (id) => {
+    const cartItem = cart.find((item) => item.id === id);
+    addToCart(cartItem, id);
+  };
+
+  const decreaseAmount = (id) => {
+    const cartItem = cart.find((item) => item.id === id);
+    if (cartItem) {
+      const newCart = cart.map((item) => {
+        if (item.id === id) {
+          return { ...item, amount: cartItem.amount - 1 };
+        } else {
+          return item;
+        }
+      });
+      setCart(newCart);
+      if (cartItem.amount < 2) {
+        removeFromCart(id);
+      }
+    }
+  };
+
+  return (
+    <CartContext.Provider
+      value={{
+        cart,
+        addToCart,
+        removeFromCart,
+        clearCart,
+        increaseAmount,
+        decreaseAmount,
+        itemAmount,
+        total,
+      }}
+    >
+      {children}
+    </CartContext.Provider>
+  );
+};
+
+export default CartProvider;
